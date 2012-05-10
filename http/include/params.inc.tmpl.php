f942908ee3019a6b798ea310c23ae8261f2bf084
psychedelys <psychedelys@googlemail.com>
2012-05-10 23:53:10 +0200

    adding the 2 first files of the concept

---
+++ http/include/params.inc.tmpl.php
@@ -0,0 +1,13 @@
+<?php
+// HTTP
+$host = "yourfabulouswebsite.exemple.org";
+$root = "/";
+// DB
+$db_driver ="pgsql";
+$db_user = "pacmiam";
+$db_pwd  = "#S0meThIng!";
+$db_server = "127.0.0.1";
+$db_name = "pacmiam_space";
+//
+$title = "Pacmiam";
+?>
