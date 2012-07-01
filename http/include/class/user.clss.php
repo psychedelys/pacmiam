3172d8e8fe021955d8fa2336c1de28c2eb0866c4
psychedelys <psychedelys@googlemail.com>
2012-07-01 22:43:58 +0200

    php tidy

--- http/include/class/user.clss.php
+++ http/include/class/user.clss.php
@@ -1,5 +1,5 @@
-<?php if (!isset($included)) die();
-
+<?php
+if (!isset($included)) die();
 /*
  ____ ___                           
  |    |   \______ ___________  ______
@@ -42,93 +42,75 @@ mysql> describe pm_usr;
 | token    | varchar(41)  | NO   |     | NULL    |                |
 +----------+--------------+------+-----+---------+----------------+
 */
-
 // Creation de l'objet user
-class	User
+class User
 {
-  public $id;  // id du User
-  public $username;  // Nom du User
-  public $password;  // Password en clair
-  public $gr44l; // Password Hachémenu 
-  public $email; // email du user
-  public $status; // status du user
-  public $token; // Token de validation
-
-
-  public $E_username;  // Flags Erreur (si true en erreur)
-  public $E_email;  // Flags Erreur (si true en erreur)
-  public $E_password;  // Flags Erreur (si true en erreur)
-  public $E_INPUT;  // Flag General erreur input
- 
-
-// Ajoute un user dans la DB
-function adduser ($username,$email,$password,$salt)
-{
-	// Verification du name du user 
-	if (valid_it($username,alphanum,4,32)) { // Le nom du user est cool
-		// Ajouter ici verif sur colision de username  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		$this->username = strtolower(mysql_real_escape_string($username));
-	} else {
-		$this->E_username = true;
-	}
-	
-	if (valid_it($email,email,4,128)) { //  on teste l'email
-
-		// Si le recorde MX du dns est ok...
-		list($part1,$domain)=split('@',$email);
-		if(checkdnsrr($domain,'MX')) {
-			$this->email = strtolower(mysql_real_escape_string($email));
-    	} else {
-    	$this->E_email = true;
-		}
-    } else {
-		$this->E_email = true;
-	}
-
-    if (valid_it($password,print_spc,8,128)) { // Validation du pwd
-		 // on s'en fout du format du sel, il est hashé de toutes facons on le vérifie pas
-$hash=$password;
-// Best practice password.. hashing * 1024 ... ralentis le dehasheur
-for ($i = 1; $i <= 1024; $i++) {
-	$hash = sha1(dechex(crc32($salt)).$hash); 
-	}
-	$this->gr44l = dechex(crc32($salt))."!".$hash; // le mysql escape sert a rien vraiment.
-     } else {
-         $this->E_password = true;
-     }
-
-     
-	if (!($this->E_username || $this->E_email || $this->E_password)) { // Si pas d'erreurs alors on fourre le user dans la db
-
-		// Géneration du token pour le mail
-		$this->token = sha1(microtime().$salt);
-
-		// connection DB
-		global $mdb2;
-
-		// Insert into DB
-		$res =& $mdb2->query(" insert into pm_usr ( username,email,gr44l,status,token) VALUES 
-		('$this->username','$this->email','$this->gr44l',0,'$this->token') 	;");   // Status 0 .. created but not valid.
-
-		// On error ..	
-		if (PEAR::isError($res)) {
-			error("SQL ERROR");
-			die($res->getMessage());
-		}
-
-		$this->id =$mdb2->lastInsertID(); // Recupere le user id
-
-       	return true; // Ca c'est bien passé
-	}
-	
-	// sinon flag "ERREUR a on" et on part false
-	$this->E_INPUT=true;
-	return false;
-
-
-}  // Fin adduser 
-
+    public $id; // id du User
+    public $username; // Nom du User
+    public $password; // Password en clair
+    public $gr44l; // Password Hachémenu
+    public $email; // email du user
+    public $status; // status du user
+    public $token; // Token de validation
+    public $E_username; // Flags Erreur (si true en erreur)
+    public $E_email; // Flags Erreur (si true en erreur)
+    public $E_password; // Flags Erreur (si true en erreur)
+    public $E_INPUT; // Flag General erreur input
+    // Ajoute un user dans la DB
+    function adduser($username, $email, $password, $salt)
+    {
+        // Verification du name du user
+        if (valid_it($username, alphanum, 4, 32)) { // Le nom du user est cool
+            // Ajouter ici verif sur colision de username  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            $this->username = strtolower(mysql_real_escape_string($username));
+        } else {
+            $this->E_username = true;
+        }
+        if (valid_it($email, email, 4, 128)) { //  on teste l'email
+            // Si le recorde MX du dns est ok...
+            list($part1, $domain) = split('@', $email);
+            if (checkdnsrr($domain, 'MX')) {
+                $this->email = strtolower(mysql_real_escape_string($email));
+            } else {
+                $this->E_email = true;
+            }
+        } else {
+            $this->E_email = true;
+        }
+        if (valid_it($password, print_spc, 8, 128)) { // Validation du pwd
+            // on s'en fout du format du sel, il est hashé de toutes facons on le vérifie pas
+            $hash = $password;
+            // Best practice password.. hashing * 1024 ... ralentis le dehasheur
+            for ($i = 1; $i <= 1024; $i++) {
+                $hash = sha1(dechex(crc32($salt)) . $hash);
+            }
+            $this->gr44l = dechex(crc32($salt)) . "!" . $hash; // le mysql escape sert a rien vraiment.
+            
+        } else {
+            $this->E_password = true;
+        }
+        if (!($this->E_username || $this->E_email || $this->E_password)) { // Si pas d'erreurs alors on fourre le user dans la db
+            // Géneration du token pour le mail
+            $this->token = sha1(microtime() . $salt);
+            // connection DB
+            global $mdb2;
+            // Insert into DB
+            $res = & $mdb2->query(" insert into pm_usr ( username,email,gr44l,status,token) VALUES 
+		('$this->username','$this->email','$this->gr44l',0,'$this->token') 	;"); // Status 0 .. created but not valid.
+            // On error ..
+            if (PEAR::isError($res)) {
+                error("SQL ERROR");
+                die($res->getMessage());
+            }
+            $this->id = $mdb2->lastInsertID(); // Recupere le user id
+            return true; // Ca c'est bien passé
+            
+        }
+        // sinon flag "ERREUR a on" et on part false
+        $this->E_INPUT = true;
+        return false;
+    } // Fin adduser
+    
 }
-
 ?>
